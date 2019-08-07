1d5c05c96fe4d103039c2fbad1dcbd91a82115f7
ANTHONY ESTIVEN FLORES BOZA <anthony.flores@ulatina.net>
2019-08-07 12:04:02 -0600

    Validacion

--- src/java/com/salesquest/controller/perfilController.java
+++ src/java/com/salesquest/controller/perfilController.java
@@ -4,15 +4,22 @@
  * and open the template in the editor.
  */
 package com.salesquest.controller;
+import com.salesquest.model.Categoria;
 import com.salesquest.model.Usuario;
 import com.salesquest.servicio.Servicio_Codigo;
 import com.salesquest.servicio.Servicio_Usuario;
 import com.salesquest.model.Codigo;
+import com.salesquest.model.EstadoCivil;
+import com.salesquest.servicio.Servicio_Categoria;
+import com.salesquest.servicio.Servicio_EstadoCivil;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.mail.Message;
@@ -21,6 +28,7 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 import javax.swing.JOptionPane;
 /**
  *
@@ -30,6 +38,131 @@ import javax.swing.JOptionPane;
 @SessionScoped
 public class perfilController {
 
+    private List<EstadoCivil> listaEstadoCivil = new ArrayList<EstadoCivil>();
+    private List<Categoria> listaCategorias = new ArrayList<Categoria>();
+    private boolean editar = true;
+    private boolean editar2 = false;
+    
+    
+    @ManagedProperty("#{loginController}")
+    private LoginController perfil;
+
+    public LoginController getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(LoginController perfil) {
+        this.perfil = perfil;
+    }
+
+    public List<EstadoCivil> getListaEstadoCivil() {
+        return listaEstadoCivil;
+    }
+
+    public void setListaEstadoCivil(List<EstadoCivil> listaEstadoCivil) {
+        this.listaEstadoCivil = listaEstadoCivil;
+    }
+
+    public List<Categoria> getListaCategorias() {
+        return listaCategorias;
+    }
+
+    public void setListaCategorias(List<Categoria> listaCategorias) {
+        this.listaCategorias = listaCategorias;
+    }
+
+    public boolean isEditar() {
+        return editar;
+    }
+
+    public void setEditar(boolean editar) {
+        this.editar = editar;
+    }
+
+    public boolean isEditar2() {
+        return editar2;
+    }
+
+    public void setEditar2(boolean editar2) {
+        this.editar2 = editar2;
+    }
+    
+    
+    
+      
+    public void redireccionarPerfil(){
+    
+        this.cargarListaEstadoCivil();
+        
+        this.cargarListaCategoria();
+        
+        
+            
+        
+        
+        this.redireccionar();
+        
+    }
+    
+    public boolean paraEditar(){
+        editar = false;
+        editar2 = false;
+        return editar;
+    }
+    
+    public void redireccionar(){
+        try {
+           
+            HttpServletRequest request = (HttpServletRequest) FacesContext
+                    .getCurrentInstance().getExternalContext().getRequest();
+            FacesContext
+                    .getCurrentInstance()
+                    .getExternalContext()
+                    .redirect(
+                            request.getContextPath()
+                            + "/faces/perfil.xhtml?faces-redirect=true");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void cargarListaCategoria(){
+        
+        Servicio_Categoria sc = new Servicio_Categoria();
+        
+        for (Object obj : sc.mostrarDatos()) {
+            this.listaCategorias.add(((Categoria)obj));
+        }
+        
+    }
+    
+    public void cargarListaEstadoCivil(){
+        
+        Servicio_EstadoCivil se = new Servicio_EstadoCivil();
+      
+        for (Object obj : se.mostrarDatos()) {
+            this.listaEstadoCivil.add((EstadoCivil)obj);
+        }
+    }
+    
+    public void completarPerfil(){
+    
+        Servicio_Usuario su = new Servicio_Usuario();
+        
+        su.actualizarDatos(this.perfil.getUsuario());
+        
+        if (this.perfil.getUsuario().getDireccion() != "") {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Correcto", "Registro Completado.");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            this.editar = true;
+            this.editar2 = true;
+                    
+        }
+        
+        
+        
+    }
+    
 }
 
 
